e1239c1b408b58383ec066c2db93a6d005cf2925
Yash Sigchi <yashsigchi@Daredevil.local>
2026-04-03 20:47:32 +0530

    Added major features

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
 import { Navbar, Welcome, Dock } from "#components";
-import { Terminal } from "#windows";
+import { Resume, Safari, Terminal ,Finder, Text, Image, Contact} from "#windows";
 import useWindowStore from "#store/window.js";
 
 // ✅ register GSAP plugins
@@ -24,6 +24,12 @@ const App = () => {
       <Dock />
 
       <Terminal />
+      <Safari />
+      <Resume />
+      <Finder />
+      <Text />
+      <Image />
+      <Contact />
     </main>
   );
 };
